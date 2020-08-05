681f863e5a018bfe9cab404153989f6d027283cc
Lee SunBowen <1554849150@qq.com>
2020-08-05 16:42:47 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,50 @@
+# 全自动更新查改 GitHub Hosts
+
+## 运行环境
+1. 需要```Python 3.0 ```以上版本以及```requests```库，您可以执行以下命令进行安装：
+	```
+	pip install requests
+	```
+2. 或者执行以下命令
+	```
+	pip install -r requirements.txt
+	```
+
+## 该方法的基本思路：
+1. 利用爬虫对GitHub各个ip地址进行更新并且在hosts文件中更新
+2. 利用Windows的计划任务实现每小时自动运行一次脚本
+
+## 具体步骤：
+1. Python自动查改hosts的脚本已经写好，即```自动查改ip地址.py```，需要下载到自己的电脑中
+2. 修改hosts需要管理员权限，方法如图：
+
+	![](./img/add_permissions.png)
+
+3. 设定计划任务，一般情况只能每天运行一次，每小时运行一次的方法参考CSDN链接：[利用windows计划任务实现每小时甚至每分钟运行一次你的Python代码](https://blog.csdn.net/kangxiaoyanl/article/details/104444534)，修改完毕后效果如图:
+
+	![](./img/Auto_Planning.png)
+
+## 注意：
+1. 在运行程序前，您务必在自己的hosts文件末尾添加如下内容，或者直接用仓库中的hosts替换您的hosts，***因为程序会根据```# GitHub Host Start```和```# GitHub Host End```文本进行定位***：
+	```
+	# GitHub Host Start
+	183.131.184.76                github.githubassets.com
+	199.232.68.133                camo.githubusercontent.com
+	199.232.68.133                github.map.fastly.net
+	199.232.69.194                github.global.ssl.fastly.net
+	140.82.112.3                  github.com
+	140.82.112.6                  api.github.com
+	199.232.68.133                raw.githubusercontent.com
+	199.232.68.133                user-images.githubusercontent.com
+	199.232.68.133                favicons.githubusercontent.com
+	199.232.68.133                avatars5.githubusercontent.com
+	199.232.68.133                avatars4.githubusercontent.com
+	199.232.68.133                avatars3.githubusercontent.com
+	199.232.68.133                avatars2.githubusercontent.com
+	199.232.68.133                avatars1.githubusercontent.com
+	199.232.68.133                avatars0.githubusercontent.com
+	140.82.114.4                  gist.github.com
+	
+	# GitHub Host End
+	```
+2. 给予程序hosts写入权限有一定的风险，如果电脑中存在木马程序，很容易对hosts文件进行修改
---
+++ 自动查改ip地址.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 13:02:56 2020
+
+@author: LiSunBowen
+"""
+
+import requests
+import re
+from collections import Counter
+
+RAW_URL = [
+    "github.githubassets.com",
+    "camo.githubusercontent.com",
+    "github.map.fastly.net",
+    "github.global.ssl.fastly.net",
+    "github.com",
+    "api.github.com",
+    "raw.githubusercontent.com",
+    "user-images.githubusercontent.com",
+    "favicons.githubusercontent.com",
+    "avatars5.githubusercontent.com",
+    "avatars4.githubusercontent.com",
+    "avatars3.githubusercontent.com",
+    "avatars2.githubusercontent.com",
+    "avatars1.githubusercontent.com",
+    "avatars0.githubusercontent.com",
+    "gist.github.com"]
+
+IPADDRESS_PREFIX = ".ipaddress.com"
+
+HOSTS_TEMPLATE = """# GitHub Host Start
+{content}
+# GitHub Host End"""
+
+def gethosts(hostsfile):
+    #读到原hosts内容
+    with open(hostsfile,'r') as fd:
+        old_hosts = fd.readlines()
+    print('已经读到原hosts文本')
+    return old_hosts
+
+def make_ipaddress_url(raw_url: str):
+    """
+    生成 ipaddress 对应的 url
+    :param raw_url: 原始 url
+    :return: ipaddress 的 url
+    """
+    dot_count = raw_url.count(".")
+    if dot_count > 1:
+        raw_url_list = raw_url.split(".")
+        tmp_url = raw_url_list[-2] + "." + raw_url_list[-1]
+        ipaddress_url = "https://" + tmp_url + IPADDRESS_PREFIX + "/" + raw_url
+    else:
+        ipaddress_url = "https://" + raw_url + IPADDRESS_PREFIX
+    return ipaddress_url
+
+
+def get_ip(session: requests.session, raw_url: str):
+    url = make_ipaddress_url(raw_url)
+    try:
+        rs = session.get(url, timeout=15)
+        pattern = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
+        ip_list = re.findall(pattern, rs.text)
+        ip_counter_obj = Counter(ip_list).most_common(1)
+        if ip_counter_obj:
+            return raw_url, ip_counter_obj[0][0]
+        raise Exception("ip address empty")
+    except Exception as ex:
+        print("get: {}, error: {}".format(url, ex))
+        raise Exception
+
+def writehosts(old_hosts, content, hostsfile):
+    #删除老hosts中的ip
+    for line in range(len(old_hosts)):
+        if old_hosts[line] == '# GitHub Host Start\n':
+            startline = line
+        elif old_hosts[line] == '# GitHub Host End':
+            endline = line
+    #得到起止行数startline，endline
+    for i in range(startline,endline+1)[::-1]:
+        del old_hosts[i]
+    #在被删除原ip地址的区域增加新的github ip
+    old_hosts.append(content)
+    #把列表内容全部连接起来
+    new_hosts = ''.join(old_hosts)
+    #复写入文件
+    with open(hostsfile,'w') as fd:
+        fd.write(new_hosts)
+    print('复写成功')
+
+def main():
+    session = requests.session()
+    content = ""
+    for raw_url in RAW_URL:
+        try:
+            host_name, ip = get_ip(session, raw_url)
+            content += ip.ljust(30) + host_name + "\n"
+        except Exception:
+            continue
+    hosts_content = HOSTS_TEMPLATE.format(content=content)
+    hostsfile = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+    old_hosts = gethosts(hostsfile)
+    writehosts(old_hosts, hosts_content, hostsfile)
+
+if __name__ == "__main__":
+    main()
